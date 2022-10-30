c20303c244b6a85933c3dd00841d896e225973df
Citizenofff <danil_shigabiev@mail.ru>
2022-10-30 11:24:02 +0500

    initial commit

---
+++ GitDZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDZ
+//
+//  Created by MacBook Air on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
